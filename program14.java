6fe316de2d1a26a0c43e70de00e7dc7bdde6075e
shailaverma05 <130858891+shailaverma05@users.noreply.github.com>
2024-03-15 00:22:26 +0530

    Update program14.java

--- program14.java
+++ program14.java
@@ -1,28 +1,26 @@
+
 //program 14
-import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 public class program14 {
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver=new ChromeDriver();
-        driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
-        String parenthandle=driver.getWindowHandle();
-        System.out.println("parenthandle:"+parenthandle);
-        driver.findElement(By.id("newWindowBtn")).click();
-        Set<String> handles=driver.getWindowHandles();
-        for(String handle:handles){
-            System.out.println(handle);
-            if(!handle.equals(parenthandle)){
-                driver.switchTo().window(handle);
-                driver.findElement(By.id("firstName")).sendKeys("shaila");
-                Thread.sleep(5000);
-                driver.close();
-            }  
-        }
-        driver.switchTo().window(parenthandle);
-        driver.findElement(By.id("name")).sendKeys("hello");
+    public static void main(String[] args) throws Exception {
+
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\Strawberry\\Documents\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        // driver.manage().window().maximize();
+        Thread.sleep(3000);
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hyrtutorials.com/p/basic-controls.html");
+        driver.findElement(By.id("firstName")).sendKeys("abcd");
+        driver.findElement(By.id("malerb")).click();
+        driver.findElement(By.id("englishchbx")).click();
+        driver.findElement(By.id("hindichbx")).click();
+        driver.findElement(By.id("registerbtn")).click();
+        driver.findElement(By.id("email")).sendKeys("123@gmail.com");
+        driver.findElement(By.linkText("Click here to navigate to the home page")).click();
         Thread.sleep(2000);
-       driver.quit();
+        driver.quit();
     }
 }
